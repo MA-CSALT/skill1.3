aab118424d54dc561ad77b4d00ec9b31ee96c344
ruiter <38767@ma-web.nl>
2025-04-08 14:48:40 +0200

    1.3.skill.3

--- M3-skilll/Assets/prefabs/bit/bit.cs
+++ M3-skilll/Assets/prefabs/bit/bit.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Bit : MonoBehaviour
 {
     // Start is called before the first frame update
 
     SpriteRenderer spriteRenderer;
-    bool state = false;
+    public bool state = false;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
